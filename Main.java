db9cf5c4d252446ebcfedb716f65602c8f0b6d52
sofiaesc07 <esc20489@uvg.edu.gt>
2022-01-18 12:01:31 -0600

    .

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static Vista vs = new Vista();
+
+    public static Boolean isOn = false;
+
+    public static void main(String[] args) { //Este método es el que inicia todo y en el que se instancian en controlador y el menú del controlador para que lleve a cabo todo el programa
+
+        
+    }
+}
--- radio.java
+++ radio.java
@@ -2,30 +2,19 @@
 /**
  * Defines all the methods that a Radio should have.
  */
-public interface Radio {
-    /**
-     * Get the current state of the radio.
-     * @return boolean indicating if the Radio is on.
-     */
-    public boolean isOn();
-    /**
-     * Switch the on state of the Radio.
-     */
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public interface Radio{
+
     public void turnOnOff();
     /**
      * Move forward into the station dial.
      * @param station boolean indicating if the frequency is AM or FM. true = AM
      */
-    public void nextStation(boolean station);
-     /**
-     * Move backwards into the station dial.
-     * @param station boolean indicating if the frequency is AM or FM. true = AM.
-     */
-    public void prevStation(boolean station);
-    /**
-     * Get the current station selected.
-     * @return double indicating the current station selected.
-     */
+
     public double getStation();
     /**
      * Save a station on the favorites array.
@@ -46,5 +35,38 @@ public interface Radio {
     /**
      * Switch the frequency selected.
      */
-    public void switchAMFM();
+
+    @Override
+    public void nextStation() {//Metodo para cambiar la emisora del radio.
+        this.emisora_actual += 0.5f;
+        System.out.println("Se ha cambiado a la emisora " + this.emisora_actual);
+    }
+
+    public void nextStation() {//Metodo para cambiar la emisora del radio.
+        this.emisora_actual += 0.5f;
+        System.out.println("Se ha cambiado a la emisora " + this.emisora_actual);
+    }
+
+    @Override
+    public void prevStation(boolean station);
+    () { //Método que guarda las emisoras y que indica cuando ya no hay espacio para guardar más.
+
+        if(this.emisoras_guardadas.size() > 50){
+            System.out.println("\nLimite de emisoras sobrepasado!\n");
+        }
+        else{this.emisoras_guardadas.add(this.emisora_actual);
+        System.out.println("Se ha guardado la emisora actual " + this.emisora_actual);}
+    }
+    @Override
+    public void CargarAEmisora() { //Método para mostrar que emisora se está reproduciendo.
+
+        System.err.println("Cargando emisora actual [" + this.modo_actual + "] " + this.emisora_actual);
+    }
+
+    public void switchAMFM(){//Metodo para cambiar la frecuencia del radio.
+
+        if(this.modo_actual.equals("AM")){this.modo_actual = "FM";}
+        else{this.modo_actual = "AM";}
+        System.out.println("Se ha cambiado al modo " + this.modo_actual);
+    }
 } 
